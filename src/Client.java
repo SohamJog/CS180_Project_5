fd9ff91236c619cbc32e21282e61c765c4e16e83
ajaybestrapalli <ajaybestra@gmail.com>
2022-12-04 14:34:34 -0500

    Check this out

--- src/Client.java
+++ src/Client.java
@@ -1,2 +1,14 @@
+import javax.swing.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
 public class Client {
+    public static void showWelcomeMessageDialog() {
+        JOptionPane.showMessageDialog(null, "Welcome to Ticket Emporium",
+                "Ticket Emporium", JOptionPane.INFORMATION_MESSAGE);
+    }
+
 }
+
