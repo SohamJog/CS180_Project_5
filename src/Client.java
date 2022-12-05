fafda1a1588abc81146eae849a6a28ce4d51dc8b
ajaybestrapalli <ajaybestra@gmail.com>
2022-12-04 20:40:09 -0500

    GUI

--- src/Client.java
+++ src/Client.java
@@ -5,10 +5,19 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 public class Client {
+
     public static void showWelcomeMessageDialog() {
         JOptionPane.showMessageDialog(null, "Welcome to Ticket Emporium",
                 "Ticket Emporium", JOptionPane.INFORMATION_MESSAGE);
-    }
+    }public static String showHostInputDialog() {
+        String university;
+       String[]  options1 = {"Sign in" , "Sign up"};
+        university = (String) JOptionPane.showInputDialog(null, "Select your option",
+                "Browser", JOptionPane.QUESTION_MESSAGE, null,options1,
+                options1[0]);
+        return university;
+    }              // returns a String of "sign in" or "sign up"
+
 
 }
 
