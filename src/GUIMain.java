8dc4813be52c97838095f709e89187181fd5c37d
Jenny Chen <jjenny0503@gmail.com>
2022-12-06 16:19:14 -0500

    GUI invokelater

--- src/GUIMain.java
+++ src/GUIMain.java
@@ -10,11 +10,6 @@ public class GUIMain extends JComponent implements Runnable {
     public static String choice;
 
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(new GUIMain());
-    }
-
-    @Override
-    public void run() {
         Socket socket = null;
         BufferedReader reader = null;
         PrintWriter writer = null;
@@ -36,7 +31,11 @@ public class GUIMain extends JComponent implements Runnable {
         }
 
         Client client = new Client();
+        SwingUtilities.invokeLater(new GUIMain());
+    }
 
+    @Override
+    public void run() {
         JFrame frame = new JFrame("Ticket Emporium");
 
         // Set the size of the frame
